7c58cbbe650f075f06a31366f5cab20dec76a227
kunguyenqt <lehoangbinhnguyen@gmail.com>
2022-12-11 23:57:46 +0700

    fix url

--- index.search.js
+++ index.search.js
@@ -4,34 +4,34 @@ var relearn_search_index = [
     "description": "",
     "tags": null,
     "title": "Design Pattern",
-    "uri": "kunguyenqt.github.io/design_pattern/index.html"
+    "uri": "/design_pattern/index.html"
   },
   {
     "content": "Design Pattern là giải pháp điển hình cho các vấn đề thường xảy ra trong thiết kế phần mềm. Chúng giống như các bản thiết kế được tạo sẵn mà bạn có thể tùy chỉnh để giải quyết vấn đề thiết kế trong mã của mình.\n",
     "description": "",
     "tags": null,
     "title": "Design Pattern là gì?",
-    "uri": "kunguyenqt.github.io/design_pattern/what/index.html"
+    "uri": "/design_pattern/what/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Categories",
-    "uri": "kunguyenqt.github.io/categories/index.html"
+    "uri": "/categories/index.html"
   },
   {
     "content": "Blog này nhằm chia sẻ các bài viết về công nghệ hoàn toàn bằng tiếng Việt. Với mong muốn nhiều người Việt Nam nhanh chóng tiếp cận với Công nghệ thông tin mà không gặp phải khó khăn về rào cản ngôn ngữ.\n",
     "description": "",
     "tags": null,
     "title": "NLKH BLOG",
-    "uri": "kunguyenqt.github.io/index.html"
+    "uri": "/index.html"
   },
   {
     "content": "",
     "description": "",
     "tags": null,
     "title": "Tags",
-    "uri": "kunguyenqt.github.io/tags/index.html"
+    "uri": "/tags/index.html"
   }
 ]
